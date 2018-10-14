2bf2fe80c817e9dc56bac67d334bc9ac8fd7508a
Thomas Bleeker <madwizard-thomas@users.noreply.github.com>
2018-10-14 15:04:48 +0200

    Minified versions Correct typing dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,64 @@
-
+import {terser} from "rollup-plugin-terser";
 import typescript from 'rollup-plugin-typescript';
+
 const env = process.env.NODE_ENV;
+const devMode = (env === 'development');
 
 const banner = "/*!\n\n webauthn-ui library (C) 2018 Thomas Bleeker (www.madwizard.org) - MIT license \n\n*/\n";
-const devMode = (env === 'development');
-export default {
-    input: './src/index.ts',
-    output: [
+
+let builds =
+    [
         {
-            file: 'dist/umd/webauthn-ui.js',
-            format: 'umd',
-            name: 'WebAuthnUI',
-            banner: banner,
-            sourcemap: devMode,
+            input: './src/index.ts',
+            output: {
+                file: 'dist/umd/webauthn-ui.js',
+                format: 'umd',
+                name: 'WebAuthnUI',
+                banner: banner,
+                sourcemap: devMode,
+            },
+            plugins: [typescript()]
         },
         {
-            file: 'dist/es/webauthn-ui.js',
-            format: 'es',
-            banner: banner,
-            sourcemap: devMode,
+            input: './src/index.ts',
+            output: {
+                file: 'dist/es/webauthn-ui.js',
+                format: 'es',
+                banner: banner,
+                sourcemap: devMode,
+            },
+            plugins: [typescript()]
         },
-    ],
 
-    plugins: [typescript()]
+    ];
 
+if (!devMode) {
+    builds = builds.concat([
+            {
+                input: './src/index.ts',
+                output: {
+                    file: 'dist/umd/webauthn-ui.min.js',
+                    format: 'umd',
+                    name: 'WebAuthnUI',
+                    banner: banner,
+                    sourcemap: devMode,
+
+                },
+                plugins: [typescript(), terser({sourcemap: devMode})]
+            },
+            {
+                input: './src/index.ts',
+                output: {
+                    file: 'dist/es/webauthn-ui.mjs',
+                    format: 'es',
+                    banner: banner,
+                    sourcemap: devMode,
+                },
+                plugins: [typescript(), terser({sourcemap: devMode})]
+            }
+
+        ]
+    );
 }
+
+export default builds;
